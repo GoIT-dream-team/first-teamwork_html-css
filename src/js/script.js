7837ec00657db47b0e9db5b956007fbe1ffcf795
Maksim Skipin <skipysmaks@gmail.com>
2020-04-10 13:39:36 +0300

    added arrow to top

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,40 @@
-$(document).ready(function() {
-  $(".top-slider").slick({
-    arrows:false,
+$(document).ready(function () {
+  // top slider
+  $('.top-slider').slick({
+    arrows: false,
     dots: true,
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     adaptiveHeight: true,
-    appendDots:$(".top-slider-dots"),
+    appendDots: $('.top-slider-dots'),
   });
 
-  $(".sec-5-slider").slick({
+  // sleder 5 section
+  $('.sec-5-slider').slick({
     dots: true,
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
-    appendDots:$(".slider-controls"),
-    appendArrows:$(".slider-controls"),
+    appendDots: $('.slider-controls'),
+    appendArrows: $('.slider-controls'),
   });
+
+  // scrollup
+  $('.scrollup').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      1000,
+    );
+  });
+});
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $('.scrollup').fadeIn();
+  }
+  else {
+    $('.scrollup').fadeOut();
+  }
 });
